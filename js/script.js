a02a68b9739eb032f449b369c8fe9c77672b7272
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-24 11:48:41 +0530

    Add functions for all basic math operators
    Normally the add, subtract, multiply, and divide operators can be found
    on most calculators, hence their functions.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(`add(3, 2): ${add(3, 2)}`)
+console.log(`subtract(3, 2): ${subtract(3, 2)}`);
+console.log(`multiply(3, 2): ${multiply(3, 2)}`);
+console.log(`divide(3, 2): ${divide(3, 2)}`);
