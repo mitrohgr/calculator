0d8e71a66f3948339806cb51ed327631f473ca0b
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-27 13:35:45 +0530

    Adjust values for window overflow with new style

--- js/script.js
+++ js/script.js
@@ -1,75 +1,89 @@
-let symbol;
-let inputValue;
-let result = 0;
-let currNumber = "";
-let operator = null;
-let prevNumber = null;
-
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll (".btn");
-
-buttons.forEach((button) => { 
-  button.addEventListener("click", handleEvent);
-});
-
-document.addEventListener("keydown", handleEvent);
-
-function handleEvent(e) {
-  let inputValue;
-  if (e.type === "click") {
-    inputValue = e.target.textContent;
-  } else if (e.type === "keydown") {
-    inputValue = e.key;
-  }
-  if (inputValue === ".") {
-    handleNumberInput(inputValue);
-  } else if (isNaN(inputValue)) {
-    handleSymbolInput(inputValue);
-  } else {
-    handleNumberInput(inputValue);
-  }
+function add(prevNumber, currNumber) {
+  return prevNumber + currNumber;
 }
 
-function handleNumberInput(inputValue) {
-  number = inputValue;
-  if (currNumber.length >= 15) {
+function subtract(prevNumber, currNumber) {
+  return prevNumber - currNumber;
+}
+
+function multiply(prevNumber, currNumber) {
+  return prevNumber * currNumber;
+}
+
+function divide(prevNumber, currNumber) {
+  return prevNumber / currNumber;
+}
+
+function operate(prevNumber, operator, currNumber) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(prevNumber, currNumber);
+      break;
+    case "-":
+      result = subtract(prevNumber, currNumber);
+      break;
+    case "*":
+      result = multiply(prevNumber, currNumber);
+      break;
+    case "/":
+      result = divide(prevNumber, currNumber);
+      break;
+  }
+  if (isNaN(result) || !isFinite(result)) {
+    return NaN;
+  }
+  if (result.toString().includes(".")) {
+    result = result.toFixed(5);
+    if (result.toString().length >= 20) {
+      return NaN;
+    }
+  }
+  if (result.toString().length >= 12) {
+    return NaN;
+  }
+  return parseFloat(result);
+}
+
+function calcBackspace() {
+  display.textContent = display.textContent.slice(0, -1);
+  currNumber = display.textContent;
+  if (currNumber === "") {
+    currNumber = "";
     return;
   }
-  switch (number) {
-    case ".":
-      if (display.textContent.includes(".")) {
-        return;
-      } else if (currNumber.length === 0) {
-        currNumber = "0.";
-      } else {
-        currNumber += ".";
-      }
-      break;
-    default:
-      currNumber += number;
-  }
-  display.textContent = currNumber;
+  currNumber = parseFloat(currNumber);
 }
 
-function handleSymbolInput(inputValue) {
-  symbol = inputValue;
-  switch (symbol) {
-    case "/": case "*":
-    case "-": case "+":
-      calcWOperator(symbol);
-      break;
-    case "=":
-      calcWEquals();
-      break;
-    case "Del":
-    case "Delete":
-      calcDelete();
-      break;
-    case "Back":
-    case "Backspace":
-      calcBackspace();
-      break;
+function calcDelete() {
+  result = 0;
+  operator = null;
+  currNumber = "";
+  prevNumber = null;
+  display.textContent = "0";
+}
+
+function calcWEquals() {
+  if (currNumber === "") {
+    if (prevNumber !== null) {
+      return;
+    }
+    return;
   }
+  if (operator === null) {
+    return;
+  }
+  prevNumber = parseFloat(prevNumber);
+  currNumber = parseFloat(currNumber);
+  result = operate(prevNumber, operator, currNumber);
+  if (isNaN(result)) {
+    display.textContent = "Funny!";
+    return;
+  } else {
+    display.textContent = result;
+  }
+  prevNumber = result;
+  currNumber = "";
 }
 
 function calcWOperator(symbol) {
@@ -99,87 +113,76 @@ function calcWOperator(symbol) {
   currNumber = "";
 }
 
-function calcWEquals() {
-  if (currNumber === "") {
-    if (prevNumber !== null) {
-      return;
-    }
+function handleSymbolInput(inputValue) {
+  symbol = inputValue;
+  switch (symbol) {
+    case "/": case "*":
+    case "-": case "+":
+      calcWOperator(symbol);
+      break;
+    case "=":
+      calcWEquals();
+      break;
+    case "Del":
+    case "Delete":
+      calcDelete();
+      break;
+    case "Back":
+    case "Backspace":
+      calcBackspace();
+      break;
+  }
+}
+
+function handleNumberInput(inputValue) {
+  number = inputValue;
+  if (currNumber.length >= 10) {
     return;
   }
-  if (operator === null) {
-    return;
+  switch (number) {
+    case ".":
+      if (display.textContent.includes(".")) {
+        return;
+      } else if (currNumber.length === 0) {
+        currNumber = "0.";
+      } else {
+        currNumber += ".";
+      }
+      break;
+    default:
+      currNumber += number;
   }
-  prevNumber = parseFloat(prevNumber);
-  currNumber = parseFloat(currNumber);
-  result = operate(prevNumber, operator, currNumber);
-  if (isNaN(result)) {
-    display.textContent = "Funny!";
-    return;
+  display.textContent = currNumber;
+}
+
+function handleEvent(e) {
+  let inputValue;
+  if (e.type === "click") {
+    inputValue = e.target.textContent;
+  } else if (e.type === "keydown") {
+    inputValue = e.key;
+  }
+  if (inputValue === ".") {
+    handleNumberInput(inputValue);
+  } else if (isNaN(inputValue)) {
+    handleSymbolInput(inputValue);
   } else {
-    display.textContent = result;
+    handleNumberInput(inputValue);
   }
-  prevNumber = result;
-  currNumber = "";
 }
 
-function calcDelete() {
-  result = 0;
-  operator = null;
-  currNumber = "";
-  prevNumber = null;
-  display.textContent = "0";
-}
+let symbol;
+let inputValue;
+let result = 0;
+let currNumber = "";
+let operator = null;
+let prevNumber = null;
 
-function calcBackspace() {
-  display.textContent = display.textContent.slice(0, -1);
-  currNumber = display.textContent;
-  if (currNumber === "") {
-    currNumber = "";
-    return;
-  }
-  currNumber = parseFloat(currNumber);
-}
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll (".btn");
 
-function operate(prevNumber, operator, currNumber) {
-  let result;
-  switch (operator) {
-    case "+":
-      result = add(prevNumber, currNumber);
-      break;
-    case "-":
-      result = subtract(prevNumber, currNumber);
-      break;
-    case "*":
-      result = multiply(prevNumber, currNumber);
-      break;
-    case "/":
-      result = divide(prevNumber, currNumber);
-      break;
-  }
-  if (isNaN(result) || !isFinite(result)) {
-    return NaN;
-  }
-  if (result.toString().includes(".")) {
-    result = result.toFixed(7);
-    if (result.toString().length >= 20) {
-      return NaN;
-    }
-  }
-  return parseFloat(result);
-}
+buttons.forEach((button) => { 
+  button.addEventListener("click", handleEvent);
+});
 
-function add(prevNumber, currNumber) {
-  return prevNumber + currNumber;
-}
-
-function subtract(prevNumber, currNumber) {
-  return prevNumber - currNumber;
-}
-
-function multiply(prevNumber, currNumber) {
-  return prevNumber * currNumber;
-}
-
-function divide(prevNumber, currNumber) {
-  return prevNumber / currNumber;
-}
+document.addEventListener("keydown", handleEvent);
